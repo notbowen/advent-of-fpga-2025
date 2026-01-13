7b63f0f75b2c8fd2079638a942d3b3885eaab485
Hu Bowen <contact@hubowen.dev>
2026-01-13 09:13:07 +0800

    rewrite this shi

--- src/aoc_solver.ml
+++ src/aoc_solver.ml
@@ -1,116 +1,223 @@
 open Base
 open Hardcaml
-open Signal
-open Always
 
-module Config = struct
-  let rows = 10
-  let cols = 10
+module type Data = sig
+  type 'a t = { value : 'a } [@@deriving hardcaml]
 end
 
-module I = struct
-  type 'a t = { clock : 'a; clear : 'a; load : 'a; load_data : 'a; start : 'a }
-  [@@deriving sexp_of, hardcaml]
+module type Config = sig
+  module Data : Data
+
+  val log_size : int
+  val width : int
+  val height : int
 end
 
-module O = struct
-  type 'a t = {
-    finished : 'a;
-    total_removed : 'a; [@bits 16]
-    current_active : 'a; [@bits 16]
-  }
-  [@@deriving sexp_of, hardcaml]
-end
+module Make (Config : Config) = struct
+  open Signal
 
-let create _scope (i : _ I.t) =
-  let open Config in
-  let spec = Reg_spec.create ~clock:i.clock ~clear:i.clear () in
+  module I = struct
+    type 'a t = { clocking : 'a Clocking.t; start : 'a; d : 'a Config.Data.t }
+    [@@deriving hardcaml]
+  end
 
-  let grid =
-    Array.init rows ~f:(fun _ ->
-        Array.init cols ~f:(fun _ -> Variable.reg spec ~width:1))
-  in
+  module O = struct
+    type 'a t = {
+      done_ : 'a;
+      total_removed : 'a; [@bits 16]
+      read_address : 'a; [@bits Config.log_size]
+      write_address : 'a; [@bits Config.log_size]
+      write_enable : 'a;
+      write_data : 'a Config.Data.t;
+    }
+    [@@deriving hardcaml]
+  end
 
-  let get_neighbour r c =
-    if r < 0 || r >= rows || c < 0 || c >= cols then gnd else grid.(r).(c).value
-  in
+  module State = struct
+    type t = Start | Load | Process | Check | Finished
+    [@@deriving sexp_of, compare ~localize, enumerate]
+  end
 
-  let removed_flags =
-    Array.init rows ~f:(fun _ ->
-        Array.init cols ~f:(fun _ -> Variable.wire ~default:gnd ()))
-  in
+  let create _scope (i : _ I.t) =
+    let spec = Clocking.to_spec i.clocking in
+    let sm = Always.State_machine.create (module State) spec in
 
-  compile
-    [
-      if_ i.load
-        (List.concat_map (List.range 0 rows) ~f:(fun r ->
-             List.init cols ~f:(fun c ->
-                 let prev_val =
-                   if r = 0 && c = 0 then i.load_data
-                   else if c = 0 then grid.(r - 1).(cols - 1).value
-                   else grid.(r).(c - 1).value
-                 in
-                 grid.(r).(c) <-- prev_val)))
+    let x_count = Clocking.Var.reg i.clocking ~width:Config.log_size in
+    let y_count = Clocking.Var.reg i.clocking ~width:Config.log_size in
+
+    let read_addr = Clocking.Var.reg i.clocking ~width:Config.log_size in
+    let total_removed = Clocking.Var.reg i.clocking ~width:16 in
+
+    (* Keep track if map has changed this round *)
+    let has_changed = Clocking.Var.reg i.clocking ~width:1 in
+
+    let enable_shift = sm.is Load |: sm.is Process in
+
+    let row0 = i.d.value in
+    let row1 = pipeline spec ~enable:enable_shift ~n:Config.width row0 in
+    let row2 = pipeline spec ~enable:enable_shift ~n:Config.width row1 in
+
+    let taps row =
+      let c0 = row in
+      let c1 = reg spec ~enable:enable_shift c0 in
+      let c2 = reg spec ~enable:enable_shift c1 in
+      [ c2; c1; c0 ]
+    in
+
+    let window = [ taps row0; taps row1; taps row2 ] in
+
+    let cur_x = pipeline spec ~n:(Config.width + 1) x_count.value in
+    let cur_y = pipeline spec ~n:(Config.width + 1) y_count.value in
+
+    let is_left = cur_x ==:. 0 in
+    let is_right = cur_x ==:. Config.width - 1 in
+    let is_top = cur_y ==:. 0 in
+    let is_bottom = cur_y ==:. Config.height - 1 in
+
+    let center = List.nth_exn (List.nth_exn window 1) 1 in
+    let neighbours_count =
+      let mask_val row col signal =
+        let is_out =
+          is_top
+          &: (of_int_trunc ~width:3 row ==:. 2)
+          |: (is_bottom &: (of_int_trunc ~width:3 row ==:. 0))
+          |: (is_left &: (of_int_trunc ~width:3 col ==:. 2))
+          |: (is_right &: (of_int_trunc ~width:3 col ==:. 0))
+        in
+        ~:is_out &: signal
+      in
+
+      List.mapi window ~f:(fun r row ->
+          List.mapi row ~f:(fun c s ->
+              if r = 1 && c = 1 then gnd else mask_val r c s))
+      |> List.concat
+      |> List.map ~f:(fun n -> uresize n ~width:4)
+      |> reduce ~f:( +: )
+    in
+
+    let remove = sm.is Process &: center &: (neighbours_count <:. 4) in
+    let write_addr = pipeline spec ~n:(Config.width + 1) read_addr.value in
+
+    Always.(
+      compile
         [
-          if_ i.start
-            (List.concat_map (List.range 0 rows) ~f:(fun r ->
-                 List.init cols ~f:(fun c ->
-                     let current_s = grid.(r).(c).value in
+          sm.switch
+            [
+              ( Start,
+                [
+                  when_ i.start
+                    [
+                      total_removed <--. 0;
+                      read_addr <--. 0;
+                      x_count <--. 0;
+                      y_count <--. 0;
+                      has_changed <-- gnd;
+                      sm.set_next Load;
+                    ];
+                ] );
+              ( Load,
+                [
+                  read_addr <-- read_addr.value +:. 1;
+                  when_
+                    (read_addr.value ==:. Config.width + 1)
+                    [ sm.set_next Process ];
+                ] );
+              ( Process,
+                [
+                  when_
+                    (read_addr.value <:. Config.width * Config.height)
+                    [ read_addr <-- read_addr.value +:. 1 ];
+                  if_
+                    (x_count.value ==:. Config.width - 1)
+                    [ x_count <--. 0; y_count <-- y_count.value +:. 1 ]
+                    [ x_count <-- x_count.value +:. 1 ];
+                  when_ remove
+                    [
+                      has_changed <-- vdd;
+                      total_removed <-- total_removed.value +:. 1;
+                    ];
+                  when_
+                    (y_count.value ==:. Config.height - 1
+                    &: (x_count.value ==:. Config.width - 1))
+                    [ sm.set_next Check ];
+                ] );
+              ( Check,
+                [
+                  if_ has_changed.value
+                    [
+                      has_changed <-- gnd;
+                      read_addr <--. 0;
+                      x_count <--. 0;
+                      y_count <--. 0;
+                      sm.set_next Load;
+                    ]
+                    [ sm.set_next Finished ];
+                ] );
+              (Finished, []);
+            ];
+        ]);
 
-                     let neighbors =
-                       [
-                         (-1, -1);
-                         (-1, 0);
-                         (-1, 1);
-                         (0, -1);
-                         (0, 1);
-                         (1, -1);
-                         (1, 0);
-                         (1, 1);
-                       ]
-                       |> List.map ~f:(fun (dr, dc) ->
-                              uresize (get_neighbour (r + dr) (c + dc)) ~width:4)
-                     in
-                     let count = reduce neighbors ~f:( +: ) in
+    {
+      O.done_ = sm.is Finished;
+      total_removed = total_removed.value;
+      read_address = read_addr.value;
+      write_address = write_addr;
+      write_enable = remove;
+      write_data = { value = gnd };
+    }
+end
 
-                     let is_accessible = current_s &: (count <:. 4) in
+module Make_with_memory (Config : Config) = struct
+  open Signal
+  module Aoc_solver = Make (Config)
 
-                     [
-                       removed_flags.(r).(c) <-- is_accessible;
-                       grid.(r).(c) <-- (current_s &: ~:is_accessible);
-                     ])
-                 |> List.concat))
-            [];
-        ];
-    ];
+  module I = struct
+    type 'a t = {
+      clocking : 'a Clocking.t;
+      start : 'a;
+      write_enable : 'a;
+      write_address : 'a; [@bits Config.log_size]
+      write_data : 'a Config.Data.t;
+    }
+    [@@deriving hardcaml]
+  end
 
-  let total_removed_this_cycle =
-    Array.to_list removed_flags
-    |> List.concat_map ~f:Array.to_list
-    |> List.map ~f:(fun v -> uresize v.value ~width:16)
-    |> reduce ~f:( +: )
-  in
+  module O = struct
+    type 'a t = { done_ : 'a; total_removed : 'a [@bits 16] }
+    [@@deriving hardcaml]
+  end
 
-  let total_removed_acc = Variable.reg spec ~width:16 in
-  compile
-    [
-      if_ i.start
-        [
-          total_removed_acc
-          <-- total_removed_acc.value +: total_removed_this_cycle;
-        ]
-        [ when_ i.clear [ total_removed_acc <--. 0 ] ];
-    ];
+  let create scope (i : _ I.t) =
+    let zero_addr = of_int_trunc ~width:Config.log_size 0 in
+    let read_wire = Config.Data.Of_signal.wires () in
 
-  let current_active =
-    Array.to_list grid
-    |> List.concat_map ~f:Array.to_list
-    |> List.map ~f:(fun v -> uresize v.value ~width:16)
-    |> reduce ~f:( +: )
-  in
+    let solver =
+      Aoc_solver.create scope
+        { Aoc_solver.I.clocking = i.clocking; start = i.start; d = read_wire }
+    in
 
-  {
-    O.finished = i.start &: (total_removed_this_cycle ==:. 0);
-    total_removed = total_removed_acc.value;
-    current_active;
-  }
+    let memory =
+      multiport_memory (1 lsl Config.log_size)
+        ~write_ports:
+          [|
+            {
+              write_clock = i.clocking.clock;
+              write_address = i.write_address;
+              write_enable = i.write_enable;
+              write_data = Config.Data.Of_signal.pack i.write_data;
+            };
+            {
+              write_clock = i.clocking.clock;
+              write_address = solver.write_address;
+              write_enable = solver.write_enable;
+              write_data = Config.Data.Of_signal.pack solver.write_data;
+            };
+          |]
+        ~read_addresses:[| zero_addr; solver.read_address |]
+    in
+
+    let solver_read_data = Config.Data.Of_signal.unpack memory.(1) in
+    Config.Data.Of_signal.assign read_wire solver_read_data;
+
+    { O.done_ = solver.done_; total_removed = solver.total_removed }
+end
--- test/test_aoc_solver.ml
+++ test/test_aoc_solver.ml
@@ -1,45 +1,70 @@
-open Base
+open Core
 open Hardcaml
-open Hardcaml_test_harness
-module Axi_wrapper = Day04.Axi_wrapper
-module Harness = Cyclesim_harness.Make (Axi_wrapper.I) (Axi_wrapper.O)
 
-let addr_ctrl = 0
-let addr_load = 1
-let addr_status = 2
-let addr_result_removed = 3
-let addr_result_active = 4
+let ( <--. ) = Bits.( <--. )
 
-let testbench sim =
-  let clear = Cyclesim.in_port sim "clear" in
+module Data = struct
+  type 'a t = { value : 'a } [@@deriving hardcaml]
+end
 
-  let write_enable = Cyclesim.in_port sim "write_enable" in
-  let write_addr = Cyclesim.in_port sim "write_addr" in
-  let write_data = Cyclesim.in_port sim "write_data" in
-  let read_addr = Cyclesim.in_port sim "read_addr" in
+module Config = struct
+  module Data = Data
 
-  let read_data = Cyclesim.out_port sim "read_data" in
+  let log_size = 7
+  let width = 10
+  let height = 10
+end
 
-  let bus_write addr data =
-    write_enable := Bits.vdd;
-    write_addr := Bits.of_int_trunc ~width:4 addr;
-    write_data := Bits.of_int_trunc ~width:32 data;
-    Cyclesim.cycle sim;
-    write_enable := Bits.gnd
-  in
+module Solver = Day04.Aoc_solver.Make_with_memory (Config)
+module Sim = Cyclesim.With_interface (Solver.I) (Solver.O)
 
-  let bus_read addr =
-    write_enable := Bits.gnd;
-    read_addr := Bits.of_int_trunc ~width:4 addr;
-    Cyclesim.cycle sim;
-    Bits.to_int_trunc !read_data
-  in
-
-  clear := Bits.vdd;
+let clear_core (sim : Sim.t) =
+  let inputs = Cyclesim.inputs sim in
+  inputs.clocking.clear <--. 1;
   Cyclesim.cycle sim;
-  clear := Bits.gnd;
+  inputs.clocking.clear <--. 0
 
-  let input_map =
+let load_inputs (sim : Sim.t) (input_data : int Data.t array) =
+  let inputs = Cyclesim.inputs sim in
+  inputs.write_enable <--. 1;
+  for i = 0 to (Config.height * Config.width) - 1 do
+    inputs.write_address <--. i;
+    inputs.write_data.value <--. input_data.(i).value;
+    Cyclesim.cycle sim
+  done
+
+let run_core (sim : Sim.t) =
+  let inputs = Cyclesim.inputs sim in
+  let outputs = Cyclesim.outputs sim in
+
+  inputs.start <--. 1;
+  Cyclesim.cycle sim;
+  inputs.start <--. 0;
+
+  let timeout = ref 0 in
+  while (not (Bits.to_bool !(outputs.done_))) && !timeout < 5000 do
+    Cyclesim.cycle sim;
+    Int.incr timeout
+  done;
+
+  Cyclesim.cycle sim;
+
+  let total_removed = Bits.to_unsigned_int !(outputs.total_removed) in
+  Stdio.printf "Total removed: %d\n" total_removed
+
+let test ~input_data =
+  let scope =
+    Scope.create ~auto_label_hierarchical_ports:true ~flatten_design:true ()
+  in
+  let sim =
+    Sim.create ~config:Cyclesim.Config.trace_all (Solver.create scope)
+  in
+  clear_core sim;
+  load_inputs sim input_data;
+  run_core sim
+
+let%expect_test "" =
+  let data =
     [
       "..@@.@@@@.";
       "@@@.@.@.@@";
@@ -52,42 +77,11 @@ let testbench sim =
       ".@@@@@@@@.";
       "@.@.@@@.@.";
     ]
+    |> List.concat_map ~f:(fun s ->
+           String.to_list s |> List.map ~f:(function '@' -> 1 | _ -> 0))
+    |> List.map ~f:(fun value -> Data.{ value })
+    |> Array.of_list
   in
 
-  let bits =
-    String.concat input_map |> String.to_list
-    |> List.map ~f:(function '@' -> 1 | _ -> 0)
-  in
-
-  List.iter bits ~f:(fun b -> bus_write addr_load b);
-
-  bus_write addr_ctrl 1;
-
-  let rec poll_until_done cycle =
-    let status_val = bus_read addr_status in
-    let is_finished = status_val land 1 = 1 in
-
-    let removed = bus_read addr_result_removed in
-    let active = bus_read addr_result_active in
-
-    Stdio.printf "[DEBUG] Cycle %d: Removed thus far = %d | Remaining = %d\n"
-      cycle removed active;
-
-    if is_finished then Stdio.printf "Answer: %d\n" removed
-    else poll_until_done (cycle + 1)
-  in
-
-  poll_until_done 1;
-
-  bus_write addr_ctrl 0
-
-let%expect_test "" =
-  Harness.run_advanced ~create:Axi_wrapper.hierarchical testbench;
-  [%expect
-    {|
-    [DEBUG] Cycle 1: Removed thus far = 25 | Remaining = 39
-    [DEBUG] Cycle 2: Removed thus far = 39 | Remaining = 31
-    [DEBUG] Cycle 3: Removed thus far = 42 | Remaining = 28
-    [DEBUG] Cycle 4: Removed thus far = 43 | Remaining = 28
-    Answer: 43
-    |}]
+  test ~input_data:data;
+  [%expect {| Total removed: 43 |}]
